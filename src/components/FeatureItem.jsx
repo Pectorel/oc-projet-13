7cf57e9128c96ab9acbf6a186130a3f82dde2530
Pectorel <contact@devadra.com>
2023-09-21 19:18:41 +0200

    Created featureItem Component

--- src/components/FeatureItem.jsx
+++ src/components/FeatureItem.jsx
@@ -1,3 +1,17 @@
-function FeatureItem() {}
+import PropTypes from "prop-types";
+import { Outlet } from "react-router-dom";
+
+FeatureItem.propTypes = {
+  img: PropTypes.string,
+  alt: PropTypes.string,
+};
+function FeatureItem({ img, alt, children }) {
+  return (
+    <div className="feature-item">
+      <img src={img} alt={alt} className="feature-icon" />
+      {children}
+    </div>
+  );
+}
 
 export default FeatureItem;
